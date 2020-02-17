e9915637a13562c32381f2738a4ed7a67a3893b5
Julia123-web <juliadesouzaesilva@gmail.com>
2020-02-17 11:27:06 +0100

    backend setup

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const user = require("./User/model");
-console.log("I WANT TO SEE", user);
+const cors = require("cors");
+const corsMiddleWare = cors();
+app.use(corsMiddleWare);
 
-const db = require("./db");
-console.log("SEE THIS", db);
+const parserMiddleWare = express.json();
+app.use(parserMiddleWare);
+
+const userRoutes = require("./User/router");
+app.use(userRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+// const user = require("./User/model");
+// console.log("I WANT TO SEE", user);
+
+// const db = require("./db");
+// console.log("SEE THIS", db);
